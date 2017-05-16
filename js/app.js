5dafa8ab67a736ba4a685dfb0600a20d31362381
stiv_grom <stanislav.kostenko@nixsolutions.com>
2017-05-16 18:50:07 +0300

    input adding

--- js/app.js
+++ js/app.js
@@ -35,7 +35,6 @@ class Article extends React.Component {
 		var data = this.props.data;
 		var articleTemplate;
 		var visible = this.state.visible; // считываем значение переменной из состояния компонента
-
 		return (
 			<div className="article">
 				<p className="news__author{index}">{data.author}:</p>
@@ -58,9 +57,19 @@ Article.propTypes = {
 }
 
 class News extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {counter: 0};
+		this.clickRead = this.clickRead.bind(this);
+	}
+	clickRead(e) {
+		e.preventDefault();
+		this.setState({counter: ++this.state.counter});
+	}
 	render() {
 		var data = this.props.data;
 		var newsTemplate;
+		var counter = this.state.counter;
 		//var data=[];
 		if (data.length > 0) {
 			newsTemplate = data.map(function(item, index) {
@@ -73,11 +82,12 @@ class News extends React.Component {
 		} else {
 			newsTemplate = <p>К сожалению новостей нет</p>
 		}
-		console.log(newsTemplate);
+		//console.log(newsTemplate);
 		return (
 			<div className="news">
 				{newsTemplate}
-				<strong className={'news__count ' + (data.length > 0 ? '':'none') }>Всего новостей: {data.length}</strong>
+				<strong onClick={this.clickRead} className={'news__count ' + (data.length > 0 ? '':'none') }>Всего новостей: {data.length}</strong>
+				<strong>Total count: {counter}</strong>
 			</div>
 		);
 	}
@@ -92,10 +102,36 @@ class Blog extends React.Component {
 		return (
 			<div className="blog">
 				Will be our blog
-</div>
+			</div>
 		);
 	}
 };
+
+class TestInput extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {myValue: ''};
+		this.changeInput = this.changeInput.bind(this);
+		this.alertClick = this.alertClick.bind(this);
+	}
+	changeInput(e) {
+		e.preventDefault();
+		this.setState({myValue: e.target.value});
+	}
+	alertClick(e) {
+		e.preventDefault();
+		alert(this.state.myValue);
+	}
+	render() {
+		return (
+			<div className="input">
+				<input className='test-input' value={this.state.myValue} onChange={this.changeInput} placeholder='введите значение' />
+				<button onClick={this.alertClick}>AAAA</button>
+			</div>
+		);
+	}
+};
+
 
 class App extends React.Component {
 	render() {
@@ -105,6 +141,7 @@ class App extends React.Component {
 				<News data={my_news} />
 				And blog:
 				<Blog />
+				<TestInput />
 			</div>
 		);
 	}
